564f9f8009906fa1e2f35804466a85e68b62ce2b
David McClure <david.mcclure@virginia.edu>
2013-06-03 08:25:51 -0400

    Adds plugin.php.

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Narrative
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+
+if (!defined('NL_NARRATIVE_DIR')) {
+    define('NL_NARRATIVE_DIR', dirname(__FILE__));
+}
+
+require_once NL_NARRATIVE_DIR . '/NeatlineNarrativePlugin.php';
+$narrative = new NeatlineNarrativePlugin();
+$narrative->setUp();
