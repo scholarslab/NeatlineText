7b10b449c1fd840b86b4c91efba6bbeddff40be8
David McClure <david.mcclure@virginia.edu>
2013-06-03 08:54:00 -0400

    Sketches in directory structure for static assets.

---
+++ views/shared/javascripts/narrative.init.js
@@ -0,0 +1,23 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Narrative
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+Neatline.module('Narrative', function(
+  Narrative, Neatline, Backbone, Marionette, $, _) {
+
+
+  Narrative.ID = 'NARRATIVE';
+
+
+  Narrative.addInitializer(function() {
+    // TODO
+  });
+
+
+});
